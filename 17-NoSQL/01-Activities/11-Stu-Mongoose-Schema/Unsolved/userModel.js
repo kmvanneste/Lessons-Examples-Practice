e76923b6a2095e98b0595cb34b9e0a7a90cad55c
kmvanneste <kmvanneste17@gmail.com>
2020-09-09 13:59:05 -0400

    practice with indexDB

--- 17-NoSQL/01-Activities/11-Stu-Mongoose-Schema/Unsolved/userModel.js
+++ 17-NoSQL/01-Activities/11-Stu-Mongoose-Schema/Unsolved/userModel.js
@@ -3,8 +3,29 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  // CODE HERE
+  username: {
+    type: String,
+    trim: true,
+    required: "String is required"
+  },
 
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+    validate: [({length}) => length >= 6, "Password must be at least 6 characters"]
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    match: [`/.+\@.+\..+/`, "Please enter a valid email"]
+  },
+
+  userCreated: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
